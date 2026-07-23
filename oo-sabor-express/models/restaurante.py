d86dfe66dbf0a031640eb81fe41d45adbc18527e
Melissa Santos <melissilva123@gmail.com>
2026-07-23 12:28:59 -0300

    Refactor Restaurante class to manage instances

--- oo-sabor-express/models/restaurante.py
+++ oo-sabor-express/models/restaurante.py
@@ -1,14 +1,22 @@
 class Restaurante:
-    nome = ""
-    categoria = ""
-    ativo = False
+    restaurantes = []
+    def __init__(this, nome, categoria, capacidade=0, avaliacao=0):
+        this.nome = nome
+        this.categoria = categoria
+        this.ativo = False
+        this.capacidade = capacidade
+        this.avaliacao = avaliacao
+        
+        Restaurante.restaurantes.append(this)
+    def __str__(this):
+        return f"{this.nome} | {this.categoria}"
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo} | {restaurante.capacidade} | {restaurante.avaliacao}")
+        
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("PizzaExpress", "Italiana")
 
-restaurante_pizza = Restaurante()
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(vars(restaurante_praca))
+Restaurante.listar_restaurantes()
