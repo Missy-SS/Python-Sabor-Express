9052f080245faec2280004ccc4e6ba3a283ad592
Melissa Santos <melissilva123@gmail.com>
2026-07-23 12:49:46 -0300

    Refactor Restaurante class attributes and methods
    Refactor Restaurante class to use private attributes and improve naming conventions.

--- oo-sabor-express/models/restaurante.py
+++ oo-sabor-express/models/restaurante.py
@@ -1,22 +1,23 @@
 class Restaurante:
     restaurantes = []
-    def __init__(this, nome, categoria, capacidade=0, avaliacao=0):
-        this.nome = nome
-        this.categoria = categoria
-        this.ativo = False
-        this.capacidade = capacidade
-        this.avaliacao = avaliacao
-        
+    def __init__(this, nome, categoria,):
+        this._nome = nome.title()
+        this._ategoria = categoria.title()
+        this._ativo = False
         Restaurante.restaurantes.append(this)
     def __str__(this):
-        return f"{this.nome} | {this.categoria}"
+        return f"{this._nome} | {this._categoria}"
     
     def listar_restaurantes():
+        print(f"{"Nome do Restaurante".ljust(25)} | {"Categoria do Resturante".ljust(25)} | {"Status do Restaurante"}")
         for restaurante in Restaurante.restaurantes:
-            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo} | {restaurante.capacidade} | {restaurante.avaliacao}")
+            print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}")
+    @property
+    def ativo(this):
+        return "✔️" if this._ativo == True else "✗"
         
 
-restaurante_praca = Restaurante("Praça", "Gourmet")
-restaurante_pizza = Restaurante("PizzaExpress", "Italiana")
+restaurante_praca = Restaurante("praça", "gourmet")
+restaurante_pizza = Restaurante("pizzaExpress", "italiana")
 
 Restaurante.listar_restaurantes()
