d1d870333e327e88041c91a69d5820c9b9d5d9ed
Melissa Santos <melissilva123@gmail.com>
2026-07-25 20:34:49 -0300

    Adição do método aplicar_desconto e anotações da aula
    Added method to apply a 5% discount on the price.

--- oo-sabor-express/models/cardapio/prato.py
+++ oo-sabor-express/models/cardapio/prato.py
@@ -4,4 +4,9 @@ class Prato(ItemCardapio):
     def __init__(this, nome, preco, descricao):
         super().__init__(nome, preco)
         this._descricao = descricao
-        
+    def aplicar_desconto(this):
+        this._preco -= (this._preco * 0.05)
+
+#super permite que acessemos informações de outras classes
+#class Prato(ItemCardapio) significa que a classe prato vai herdar atributos da classe ItemCardapio
+#quando um método é moldado de forma diferente em diferentes classes: Polimorfismo
