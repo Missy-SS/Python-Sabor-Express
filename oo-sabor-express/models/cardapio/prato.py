54434c382fe1214658155be0289f621bbc96c6bd
Melissa Santos <melissilva123@gmail.com>
2026-07-24 22:23:39 -0300

    Extend Prato class to inherit from ItemCardapio

--- oo-sabor-express/models/cardapio/prato.py
+++ oo-sabor-express/models/cardapio/prato.py
@@ -1,3 +1,7 @@
-class Prato:
+from models.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
     def __init__(this, nome, preco, descricao):
-        pass
+        super().__init__(nome, preco)
+        this._descricao = descricao
+        
