f54df682348cfd75109de4fee71149372ca1bfc1
Melissa Santos <melissilva123@gmail.com>
2026-07-25 20:35:50 -0300

    Adição do método especial __str__ para bebida e do método aplicar_desconto
    Added string representation and discount method.

--- oo-sabor-express/models/cardapio/bebida.py
+++ oo-sabor-express/models/cardapio/bebida.py
@@ -4,3 +4,9 @@ class Bebida(ItemCardapio):
     def __init__(this, nome, preco, tamanho):
         super().__init__(nome, preco)
         this._tamanho = tamanhopass
+        
+    def __str__(this):
+        return this._nome
+    
+    def aplicar_desconto(this):
+        this._preco -= (this._preco * 0.08)
